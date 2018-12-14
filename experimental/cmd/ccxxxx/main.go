959b9acf6419acd90c6c50d20a4356565d7ff8c4
John Maguire <john.maguire@gmail.com>
2018-12-14 17:28:53 +0000

    Initial support for cc1101

---
+++ experimental/cmd/ccxxxx/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/hex"
+	"flag"
+	"log"
+
+	"periph.io/x/periph/conn/gpio/gpioreg"
+	"periph.io/x/periph/conn/spi/spireg"
+	"periph.io/x/periph/experimental/devices/ccxxxx"
+	"periph.io/x/periph/host"
+)
+
+func main() {
+	flag.Parse()
+	host.Init()
+	port, err := spireg.Open("")
+	if err != nil {
+		log.Fatalf("spireg.Open: %v", err)
+	}
+
+	gdo0 := gpioreg.ByName("25")
+	gdo2 := gpioreg.ByName("24")
+
+	dev, err := ccxxxx.New(port, gdo0, gdo2)
+	if err != nil {
+		log.Fatalf("ccxxxx: %v", err)
+	}
+
+	dev.Config(ccxxxx.Config_868_3)
+
+	for {
+		packet, err := dev.Receive(-1)
+		if err != nil {
+			log.Printf("Error receiving packet: %v", err)
+		} else {
+			log.Printf("RSSI: %d LQI %d CRC: %v", packet.RSSI, packet.LQI, packet.CRC)
+			log.Println(hex.Dump(packet.Data))
+		}
+	}
+}
