3640be7496b99e4106a6e088add4a73c92f19f40
John Maguire <john.maguire@gmail.com>
2018-12-17 20:05:21 +0000

    Add test

---
+++ experimental/devices/ccxxxx/ccxxxx_test.go
@@ -0,0 +1,15 @@
+package ccxxxx
+
+import (
+	"bytes"
+	"testing"
+
+	"periph.io/x/periph/conn/physic"
+)
+
+func TestCCxxxx_calculateFreq(t *testing.T) {
+	f := calculateFreq(26 * physic.MegaHertz, 868300 * physic.KiloHertz)
+	if !bytes.Equal([]byte{0x21, 0x65, 0x6a}, f) {
+		t.Fatalf("calculateFreq() = %v != %v", f, 0x21656a)
+	}
+}
